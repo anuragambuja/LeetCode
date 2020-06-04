0d63c28a0274982cee85557804749233ecfc6539
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-06-05 02:13:52 +0530

    Update 283. Move Zeroes.py

--- Easy/283. Move Zeroes.py
+++ Easy/283. Move Zeroes.py
@@ -44,3 +44,16 @@ class Solution:
         while(i < size):
             nums[i] = 0
             i+=1                
+
+ class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        pos = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
